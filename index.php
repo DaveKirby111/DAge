4878a37d42b616472b914368e1d26b966e26d52b
DaveKirby111 <dkirby111@aol.com>
2022-12-27 14:32:33 -0600

    started project

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php require "header.php"; ?>
+
+<div class="mobilenav">
+
+    <div class="topnav">
+        <a href="index.php" class="home">dragon age fansite</a>
+
+
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+            </a>
+
+            <div id="nav">
+                <a href="">link</a>
+                <a href="">link</a>
+                <a href="">link</a>
+            </div>
+    </div>
+
+</div>
+
+<nav>
+    <div class="title">
+    <img src="resources/images/title.jpg" alt="title"
+    style="height: 45px; width: 150px;">
+    <p>fansite</p>
+    </div>
+    
+    <ul>
+        <li><a href="">link</a></li>
+        <li><a href="">link</a></li>
+        <li><a href="">link</a></li>
+    </ul>
+</nav>
+
+<header>
+    <img src="resources/images/dage-logo.jpg" alt="logo">
+    <h1>homepage</h1>
+</header>
+
+<div class="container" 
+style="background-image: url('resources/images/D-age.jpg');
+background-size: 100% 100%;">
+
+    <main>
+
+    <div class="content">
+        <h2>intro</h2>
+    </div>
+
+    </main>
+</div>
+
+<?php require "footer.php"; ?>
