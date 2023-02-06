48dd66abd979f1cb20aff26ff9f69f50743acf0a
David Kirby <david@kinetic.com>
2023-02-06 11:47:23 -0600

    redid nav, added trailer page, added video

--- partials/nav.php
+++ partials/nav.php
@@ -1,24 +1,3 @@
-<div class="mobilenav">
-
-    <div class="topnav">
-        <a href="index.php" class="home">dragon age fansite</a>
-
-
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-            </a>
-
-            <div id="nav">
-                <a href="index.php">home</a>
-                <a href="story.php">story</a>
-                <a href="chars.php">characters</a>
-                <a href="gallery.php">gallery</a>
-                <a href="comments.php">comments</a>
-            </div>
-    </div>
-
-</div>
-
 <nav>
     <div class="title">
     <img src="resources/images/title.jpg" alt="title"
@@ -27,10 +6,23 @@
     </div>
     
     <ul>
-        <li><a href="index.php">home</a></li>
-        <li><a href="story.php">story</a></li>
-        <li><a href="chars.php">characters</a></li>
-        <li><a href="gallery.php">gallery</a></li>
-        <li><a href="comments.php">comments</a></li>
+        
+    <?php
+
+    $nav = [
+        "/" => "Home",
+        "trailer.php" => "Trailer",
+        "story.php" => "Story",
+        "chars.php" => "Characters",
+        "gallery.php" => "Gallery",
+        "comments.php" => "Comments"
+    ];
+
+    foreach ($nav as $key => $link) {
+        echo '<li><a href="'.$key.'">'.$link.'</a></li>';
+    }
+
+    ?>
+
     </ul>
 </nav>
---
+++ trailer.php
@@ -0,0 +1,31 @@
+<?php require "partials/header.php"; ?>
+
+<?php require "partials/nav.php"; ?>
+
+<?php
+
+$heading = "trailer";
+
+?>
+
+<?php require "partials/banner.php"; ?>
+
+<div class="container" 
+style="background-image: url('resources/images/D-age.jpg');">
+
+    <main>
+
+    <div class="content">
+
+        <h2>Sacred Ashes</h2>
+
+        <video width="1080" controls>
+            <source src="resources/media/Sacred Ashes 2.mp4">
+        </video>
+       
+    </div>
+
+    </main>
+</div>
+
+<?php require "partials/footer.php"; ?>
